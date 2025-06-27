40ccb006d523b1d68810277487d4c016c2fb7571
Santosh Kurmi <xantosh121@gmail.com>
2025-06-27 17:45:09 +0545

    almsot completed again

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, HttpServer};
-use builder::{build::{abort::{self, abort, abort_all}, build_init::build_initialize}, models::{app_state::{AppState, ChannelMessage}, config::Config}, refresh::handle_refresh::handle_error_refresh, socket::{handle_socket::connect_and_stream_ws, handle_socket_project::connect_and_stream_ws_project}};
+use app_builder::socket::valid_project_token::is_valid_project_token;
+use app_builder::{build::{abort::{abort, abort_all}, build_init::build_initialize}, models::{app_state::AppState, config::Config}, pending_update::get_pending_update::get_pending_update, socket::{handle_socket::connect_and_stream_ws_build, handle_socket_project::connect_and_stream_ws_project}};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[actix_web::main]
@@ -26,12 +27,13 @@ async fn main() -> std::io::Result<()> {
     let server = HttpServer::new(move || {
         let  app = App::new()
             .app_data(app_data.clone())
-            .service(web::resource("/api/build").route(web::post().to(build_initialize)))
-            .service(web::resource("/api/build/connect").route(web::get().to(connect_and_stream_ws)))
-            .service(web::resource("/api/project/connect").route(web::get().to(connect_and_stream_ws_project)))
-            .service(web::resource("/api/project/abort").route(web::post().to(abort_all)))
-            .service(web::resource("/api/build/abort").route(web::post().to(  abort  )))
-            .service(web::resource("/api/errors").route(web::post().to(  handle_error_refresh  )))
+            .service(web::resource("/api/init/build").route(web::post().to(build_initialize)))
+            .service(web::resource("/api/connect/build").route(web::get().to(connect_and_stream_ws_build)))
+            .service(web::resource("/api/connect/project").route(web::get().to(connect_and_stream_ws_project)))
+            .service(web::resource("/api//abort/all").route(web::post().to(abort_all)))
+            .service(web::resource("/api/abort").route(web::post().to(  abort  )))
+            .service(web::resource("/api/pending/updates").route(web::get().to(  get_pending_update  )))
+            .service(web::resource("/api/isvalid/token").route(web::get().to(  is_valid_project_token  )))
             ;
         app
     });
